431ddba37abdb12f7b895a72a4de0cca3f2061fb
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-12-13 10:08:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,6 @@ function handleStart(evt) {
   for (let i = 0; i < touches.length; i++) {
     log(`touchstart: ${i}.`);
     ongoingTouches.push(copyTouch(touches[i]));
-    const color = colorForTouch(touches[i]);
-    log(`color of touch with id ${touches[i].identifier} = ${color}`);
-    ctx.beginPath();
-    ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false); // 最初に円を描く
-    ctx.fillStyle = color;
-    ctx.fill();
   }
 }
 
@@ -42,16 +36,10 @@ function handleMove(evt) {
 
     if (idx >= 0) {
       log(`continuing touch ${idx}`);
-      ctx.beginPath();
       log(
         `ctx.moveTo( ${ongoingTouches[idx].pageX}, ${ongoingTouches[idx].pageY} );`,
       );
-      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
       log(`ctx.lineTo( ${touches[i].pageX}, ${touches[i].pageY} );`);
-      ctx.lineTo(touches[i].pageX, touches[i].pageY);
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = color;
-      ctx.stroke();
 
       ongoingTouches.splice(idx, 1, copyTouch(touches[i])); // swap in the new touch record
     } else {
@@ -64,20 +52,12 @@ function handleEnd(evt) {
   evt.preventDefault();
   log("touchend");
   const el = document.getElementById("canvas");
-  const ctx = el.getContext("2d");
   const touches = evt.changedTouches;
 
   for (let i = 0; i < touches.length; i++) {
-    const color = colorForTouch(touches[i]);
     let idx = ongoingTouchIndexById(touches[i].identifier);
 
     if (idx >= 0) {
-      ctx.lineWidth = 4;
-      ctx.fillStyle = color;
-      ctx.beginPath();
-      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-      ctx.lineTo(touches[i].pageX, touches[i].pageY);
-      ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8); // and a square at the end
       ongoingTouches.splice(idx, 1); // remove it; we're done
     } else {
       log("can't figure out which touch to end");
